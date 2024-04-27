eaebee1074a7f9eb0d0d8c6fbddfaedb116373c6
dosangdev <sangdo1113@naver.com>
2024-04-27 15:38:28 +0900

    캘린더 html, css적용

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,104 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { DayPicker } from "react-day-picker";
-import "react-day-picker/dist/style.css";
+import AttendanceModal from "@/components/Modal/AttendanceModal";
+import { useEffect, useState, useRef } from "react";
+import { DayProps, DayPicker } from "react-day-picker";
+
+type TagProps = {
+  text: string;
+  showModalParticipationList: boolean;
+  setShowModalParticipationList: boolean;
+};
+
+function Tag({
+  text,
+}: // setShowModalParticipationList,
+// showModalParticipationList,
+TagProps) {
+  // console.log(showModalParticipationList);
+
+  return (
+    <div className="flex gap-x-10">
+      <button
+      // onClick={() =>
+      //   setShowModalParticipationList(!showModalParticipationList)
+      // }
+      >
+        {text} (0)
+      </button>
+      <button className="w-11 bg-blue-600 text-white font-thin rounded-[50%]">
+        참가
+      </button>
+    </div>
+  );
+}
+
+function CustomDay(props: DayProps) {
+  console.log(props.date);
+  return (
+    <div
+      className={`pl-2 flex flex-col	justify-around h-[100%] ${
+        new Date().getMonth() + 1 !== props.date.getMonth() + 1
+          ? "text-[#ebebe4] cursor-none"
+          : ""
+      } `}
+    >
+      <div>{props.date.getDate()}</div>
+      <Tag
+        // showModalParticipationList={showModalParticipationList}
+        // setShowModalParticipationList={setShowModalParticipationList}
+        text="(오전) 07 ~ 12"
+      />
+      <Tag
+        // showModalParticipationList={showModalParticipationList}
+        // setShowModalParticipationList={setShowModalParticipationList}
+        text="(오후) 12 ~ 18"
+      />
+      <Tag
+        // showModalParticipationList={showModalParticipationList}
+        // setShowModalParticipationList={setShowModalParticipationList}
+        text="(오후) 18 ~ 00"
+      />
+    </div>
+  );
+}
 
 const DatePickerComponent: React.FC = () => {
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
   const initialDays: Date[] = [];
   const [days, setDays] = useState<Date[] | undefined>(initialDays);
-  const [modify, setModify] = useState(true);
-
-  let footer =
-    days && days.length > 0 ? (
-      <p>You selected {days.length} day(s).</p>
-    ) : (
-      <p>Please pick one or more days.</p>
-    );
-
-  useEffect(() => {
-    console.log(days?.length);
-  }, [days]);
+  const [showModalParticipationList, setShowModalParticipationList] =
+    useState<boolean>(false);
 
   return (
     <>
+      {/* className={`pl-2 flex flex-col	justify-around h-[100%] ${
+        new Date().getMonth() + 1 !== props.date.getMonth() + 1
+          ? "text-[#ebebe4] cursor-none"
+          : ""
+      } `} */}
+
+      {isModalOpen ? (
+        <AttendanceModal
+          isModalOpen={isModalOpen}
+          setIsModalOpen={setIsModalOpen}
+        />
+      ) : (
+        ""
+      )}
+
       <DayPicker
+        components={{
+          Day: CustomDay,
+        }}
         mode="multiple"
         selected={days}
         onSelect={setDays}
-        footer={footer}
-        disabled={modify}
       />
-      <div>
-        <button onClick={() => setModify(!modify)}>
-          {modify && days?.length === 0 ? "추가" : modify ? "수정" : "완료"}
-        </button>
-      </div>
+      <button onClick={() => setIsModalOpen(!isModalOpen)}>
+        버튼!!!!!!!!!!
+      </button>
     </>
   );
 };
---
+++ components/Modal/AttendanceModal.tsx
@@ -0,0 +1,91 @@
+import { useState } from "react";
+
+type AttendanceModalProps = {
+  isModalOpen?: boolean;
+  setIsModalOpen: (isOpen: boolean) => void;
+};
+
+export default function AttendanceModal(props: AttendanceModalProps) {
+  function closeModal() {
+    props.setIsModalOpen(false);
+    console.log("closeModal");
+  }
+
+  function handleModalClick(e: React.MouseEvent<HTMLDivElement>) {
+    e.stopPropagation();
+  }
+
+  let dummyParticipationList = [
+    {
+      name: "김상도",
+      qqq: "111",
+    },
+    {
+      name: "박찬우",
+      qqq: "111",
+    },
+    {
+      name: "사공용협",
+      qqq: "111",
+    },
+    {
+      name: "홍길동",
+      qqq: "111",
+    },
+    {
+      name: "김상도",
+      qqq: "111",
+    },
+  ]; // 더미데이터
+
+  return (
+    <>
+      <div
+        className="inset-0 w-[100%] h-[100%] fixed bg-[#000000] opacity-40"
+        onClick={closeModal}
+      ></div>
+      <div
+        className="inset-0 fixed flex justify-center items-center"
+        onClick={closeModal}
+      >
+        <div
+          className="w-[800px] h-[800px] bg-[#FFFFFF] rounded-lg shadow-lg overflow-hidden z-20 "
+          onClick={handleModalClick}
+        >
+          {/* 모달 헤더 */}
+          <div className="px-6 py-4 border-b border-gray-200">
+            <h3 className="text-lg leading-6 font-medium text-gray-900">
+              참가자 명단
+            </h3>
+          </div>
+
+          {/* 모달 바디 */}
+          <div className="p-6">
+            <ul className="divide-y divide-gray-200">
+              {dummyParticipationList.map((participant, index) => (
+                <li key={index} className="py-4">
+                  <div className="flex items-center space-x-3">
+                    <div className="text-sm font-medium text-gray-900">
+                      {participant.name}
+                    </div>
+                    {/* 참가자별 추가 정보가 있다면 여기에 표시 */}
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+          {/* 모달 푸터 */}
+          <div className="px-6 py-4 bg-gray-50 text-right">
+            <button
+              onClick={closeModal}
+              type="button"
+              className="py-2 px-4 bg-gray-600 text-white rounded hover:bg-gray-700 focus:outline-none focus:bg-gray-700"
+            >
+              닫기
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
